a2d60c58977323697dcc01810d94a32649575e63
Guillermo Rauch <rauchg@gmail.com>
2017-02-16 01:51:05 -0300

    proof of concept xterm.js

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,10 @@ module.exports = {
       {
         test: /\.json/,
         loader: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
       }
     ]
   },
